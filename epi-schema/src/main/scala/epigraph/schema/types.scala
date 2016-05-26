07d8c684ec054ff8b8edfcc6412470f59e6f2e3d
Yegor Borovikov <yegor@sumologic.com>
2016-05-26 12:36:45 -0700

    Adding the rest of schema types to scala bindings for schema

--- epi-schema/src/main/scala/epigraph/schema/types.scala
+++ epi-schema/src/main/scala/epigraph/schema/types.scala
@@ -193,7 +193,7 @@ object TypeMemberRef extends MultiVarType[TypeMemberRef](ns \ "TypeMemberRef", S
 trait DataTypeData extends TypeData with RecordDatum[DataTypeData]
 
 
-object DataTypeData extends RecordType[DataTypeData](ns \ "DataTypeData", Seq(TypeData)) {
+object DataTypeData extends RecordType[DataTypeData](ns \ "DataTypeData", Seq(TypeData), true) {
 
   val _supertypes: DatumField[ListDatum[DataTypeRef]] = field("supertypes", DataTypeRef.listOf)
 
@@ -297,3 +297,349 @@ object FieldData extends RecordType[FieldData](ns \ "FieldData", Seq(Named)) {
   override lazy val declaredFields: DeclaredFields = DeclaredFields(_name, _valueType, _default)
 
 }
+
+
+/**
+ * {{{
+ * record MapTypeData extends DataTypeData {
+ *   override supertypes: list[MapTypeRef]
+ *   keyType: DataTypeRef
+ *   valueType: VarTypeRef
+ * }
+ * }}}
+ */
+trait MapTypeData extends DataTypeData with RecordDatum[MapTypeData]
+
+
+object MapTypeData extends RecordType[MapTypeData](ns \ "MapTypeData", Seq(DataTypeData)) {
+
+  val _supertypes: DatumField[ListDatum[MapTypeRef]] = field("supertypes", MapTypeRef.listOf)
+
+  val _keyType: VarTagField[DataTypeRef, ByNameRef.`"name"`.type, QualifiedTypeName] =
+    field[DataTypeRef, ByNameRef.`"name"`.type, QualifiedTypeName]("keyType", DataTypeRef, DataTypeRef._name)
+
+  val _valueType: VarTagField[VarTypeRef, ByNameRef.`"name"`.type, QualifiedTypeName] =
+    field[VarTypeRef, ByNameRef.`"name"`.type, QualifiedTypeName]("valueType", VarTypeRef, VarTypeRef._name)
+
+  override lazy val declaredFields: DeclaredFields = DeclaredFields(_supertypes, _keyType, _valueType)
+
+}
+
+
+/**
+ * {{{
+ * vartype MapTypeRef extends DataTypeRef { type: MapTypeData }
+ * }}}
+ */
+trait MapTypeRef extends DataTypeRef with Var[MapTypeRef]
+
+
+object MapTypeRef extends MultiVarType[MapTypeRef](ns \ "MapTypeRef", Seq(DataTypeRef)) {
+
+  val _type: Tag[TypeRef.`"type"`.type, MapTypeData] = tag(TypeRef.`"type"`, MapTypeData)
+
+  override lazy val declaredVarTags: DeclaredTags = DeclaredTags(_type)
+
+}
+
+
+/**
+ * {{{
+ * record ListTypeData extends DataTypeData {
+ *   override supertypes: list[ListTypeRef]
+ *   valueType: VarTypeRef
+ * }
+ * }}}
+ */
+trait ListTypeData extends DataTypeData with RecordDatum[ListTypeData]
+
+
+object ListTypeData extends RecordType[ListTypeData](ns \ "ListTypeData", Seq(DataTypeData)) {
+
+  val _supertypes: DatumField[ListDatum[ListTypeRef]] = field("supertypes", ListTypeRef.listOf)
+
+  val _valueType: VarTagField[VarTypeRef, ByNameRef.`"name"`.type, QualifiedTypeName] =
+    field[VarTypeRef, ByNameRef.`"name"`.type, QualifiedTypeName]("valueType", VarTypeRef, VarTypeRef._name)
+
+  override lazy val declaredFields: DeclaredFields = DeclaredFields(_supertypes, _valueType)
+
+}
+
+
+/**
+ * {{{
+ * vartype ListTypeRef extends DataTypeRef { type: ListTypeData }
+ * }}}
+ */
+trait ListTypeRef extends DataTypeRef with Var[ListTypeRef]
+
+
+object ListTypeRef extends MultiVarType[ListTypeRef](ns \ "ListTypeRef", Seq(DataTypeRef)) {
+
+  val _type: Tag[TypeRef.`"type"`.type, ListTypeData] = tag(TypeRef.`"type"`, ListTypeData)
+
+  override lazy val declaredVarTags: DeclaredTags = DeclaredTags(_type)
+
+}
+
+
+/**
+ * {{{
+ * record EnumTypeData extends DataTypeData {
+ *   override supertypes: list[EnumTypeRef]
+ * //valueType: DataTypeRef?
+ *   values: list[EnumValueData]
+ * }
+ * }}}
+ */
+trait EnumTypeData extends DataTypeData with RecordDatum[EnumTypeData]
+
+
+object EnumTypeData extends RecordType[EnumTypeData](ns \ "EnumTypeData", Seq(DataTypeData)) {
+
+  val _supertypes: DatumField[ListDatum[EnumTypeRef]] = field("supertypes", EnumTypeRef.listOf)
+
+  val _values: DatumField[ListDatum[EnumValueData]] = field("values", EnumValueData.listOf)
+
+  override lazy val declaredFields: DeclaredFields = DeclaredFields(_supertypes, _values)
+
+}
+
+
+/**
+ * {{{
+ * vartype EnumTypeRef extends DataTypeRef { override type: EnumTypeData }
+ * }}}
+ */
+trait EnumTypeRef extends DataTypeRef with Var[EnumTypeRef]
+
+
+object EnumTypeRef extends MultiVarType[EnumTypeRef](ns \ "EnumTypeRef", Seq(DataTypeRef)) {
+
+  val _type: Tag[TypeRef.`"type"`.type, EnumTypeData] = tag(TypeRef.`"type"`, EnumTypeData)
+
+  override lazy val declaredVarTags: DeclaredTags = DeclaredTags(_type)
+
+}
+
+
+/**
+ * {{{
+ * record EnumValueData extends Named { override name: EnumValueName } // TODO value??
+ * }}}
+ */
+trait EnumValueData extends Named with RecordDatum[EnumValueData]
+
+
+object EnumValueData extends RecordType[EnumValueData](ns \ "EnumValueData", Seq(Named)) {
+
+  val _name: DatumField[EnumValueName] = field("name", EnumValueName)
+
+  override lazy val declaredFields: DeclaredFields = DeclaredFields(_name)
+
+}
+
+
+/**
+ * {{{
+ * abstract polymorphic record PrimitiveTypeData extends DataTypeData { override supertypes: list[PrimitiveTypeRef] }
+ * }}}
+ */
+trait PrimitiveTypeData extends DataTypeData with RecordDatum[PrimitiveTypeData]
+
+
+object PrimitiveTypeData extends RecordType[PrimitiveTypeData](ns \ "PrimitiveTypeData", Seq(DataTypeData), true) {
+
+  val _supertypes: DatumField[ListDatum[PrimitiveTypeRef]] = field("supertypes", PrimitiveTypeRef.listOf)
+
+  override lazy val declaredFields: DeclaredFields = DeclaredFields(_supertypes)
+
+}
+
+
+/**
+ * {{{
+ * vartype PrimitiveTypeRef extends DataTypeRef { override type: PrimitiveTypeData }
+ * }}}
+ */
+trait PrimitiveTypeRef extends DataTypeRef with Var[PrimitiveTypeRef]
+
+
+object PrimitiveTypeRef extends MultiVarType[PrimitiveTypeRef](ns \ "PrimitiveTypeRef", Seq(DataTypeRef)) {
+
+  val _type: Tag[TypeRef.`"type"`.type, PrimitiveTypeData] = tag(TypeRef.`"type"`, PrimitiveTypeData)
+
+  override lazy val declaredVarTags: DeclaredTags = DeclaredTags(_type)
+
+}
+
+
+/**
+ * {{{
+ * record StringTypeData extends PrimitiveTypeData { override supertypes: list[StringTypeRef] }
+ * }}}
+ */
+trait StringTypeData extends PrimitiveTypeData with RecordDatum[StringTypeData]
+
+
+object StringTypeData extends RecordType[StringTypeData](ns \ "StringTypeData", Seq(PrimitiveTypeData), true) {
+
+  val _supertypes: DatumField[ListDatum[StringTypeRef]] = field("supertypes", StringTypeRef.listOf)
+
+  override lazy val declaredFields: DeclaredFields = DeclaredFields(_supertypes)
+
+}
+
+
+/**
+ * {{{
+ * vartype StringTypeRef extends PrimitiveTypeRef { override type: StringTypeData }
+ * }}}
+ */
+trait StringTypeRef extends PrimitiveTypeRef with Var[StringTypeRef]
+
+
+object StringTypeRef extends MultiVarType[StringTypeRef](ns \ "StringTypeRef", Seq(DataTypeRef)) {
+
+  val _type: Tag[TypeRef.`"type"`.type, StringTypeData] = tag(TypeRef.`"type"`, StringTypeData)
+
+  override lazy val declaredVarTags: DeclaredTags = DeclaredTags(_type)
+
+}
+
+
+/**
+ * {{{
+ * record IntegerTypeData extends PrimitiveTypeData { override supertypes: list[IntegerTypeRef] }
+ * }}}
+ */
+trait IntegerTypeData extends PrimitiveTypeData with RecordDatum[IntegerTypeData]
+
+
+object IntegerTypeData extends RecordType[IntegerTypeData](ns \ "IntegerTypeData", Seq(PrimitiveTypeData), true) {
+
+  val _supertypes: DatumField[ListDatum[IntegerTypeRef]] = field("supertypes", IntegerTypeRef.listOf)
+
+  override lazy val declaredFields: DeclaredFields = DeclaredFields(_supertypes)
+
+}
+
+
+/**
+ * {{{
+ * vartype IntegerTypeRef extends PrimitiveTypeRef { override type: IntegerTypeData }
+ * }}}
+ */
+trait IntegerTypeRef extends PrimitiveTypeRef with Var[IntegerTypeRef]
+
+
+object IntegerTypeRef extends MultiVarType[IntegerTypeRef](ns \ "IntegerTypeRef", Seq(DataTypeRef)) {
+
+  val _type: Tag[TypeRef.`"type"`.type, IntegerTypeData] = tag(TypeRef.`"type"`, IntegerTypeData)
+
+  override lazy val declaredVarTags: DeclaredTags = DeclaredTags(_type)
+
+}
+
+
+/**
+ * {{{
+ * record LongTypeData extends PrimitiveTypeData { override supertypes: list[LongTypeRef] }
+ * }}}
+ */
+trait LongTypeData extends PrimitiveTypeData with RecordDatum[LongTypeData]
+
+
+object LongTypeData extends RecordType[LongTypeData](ns \ "LongTypeData", Seq(PrimitiveTypeData), true) {
+
+  val _supertypes: DatumField[ListDatum[LongTypeRef]] = field("supertypes", LongTypeRef.listOf)
+
+  override lazy val declaredFields: DeclaredFields = DeclaredFields(_supertypes)
+
+}
+
+
+/**
+ * {{{
+ * vartype LongTypeRef extends PrimitiveTypeRef { override type: LongTypeData }
+ * }}}
+ */
+trait LongTypeRef extends PrimitiveTypeRef with Var[LongTypeRef]
+
+
+object LongTypeRef extends MultiVarType[LongTypeRef](ns \ "LongTypeRef", Seq(DataTypeRef)) {
+
+  val _type: Tag[TypeRef.`"type"`.type, LongTypeData] = tag(TypeRef.`"type"`, LongTypeData)
+
+  override lazy val declaredVarTags: DeclaredTags = DeclaredTags(_type)
+
+}
+
+
+/**
+ * {{{
+ * record DoubleTypeData extends PrimitiveTypeData { override supertypes: list[DoubleTypeRef] }
+ * }}}
+ */
+trait DoubleTypeData extends PrimitiveTypeData with RecordDatum[DoubleTypeData]
+
+
+object DoubleTypeData extends RecordType[DoubleTypeData](ns \ "DoubleTypeData", Seq(PrimitiveTypeData), true) {
+
+  val _supertypes: DatumField[ListDatum[DoubleTypeRef]] = field("supertypes", DoubleTypeRef.listOf)
+
+  override lazy val declaredFields: DeclaredFields = DeclaredFields(_supertypes)
+
+}
+
+
+/**
+ * {{{
+ * vartype DoubleTypeRef extends PrimitiveTypeRef { override type: DoubleTypeData }
+ * }}}
+ */
+trait DoubleTypeRef extends PrimitiveTypeRef with Var[DoubleTypeRef]
+
+
+object DoubleTypeRef extends MultiVarType[DoubleTypeRef](ns \ "DoubleTypeRef", Seq(DataTypeRef)) {
+
+  val _type: Tag[TypeRef.`"type"`.type, DoubleTypeData] = tag(TypeRef.`"type"`, DoubleTypeData)
+
+  override lazy val declaredVarTags: DeclaredTags = DeclaredTags(_type)
+
+}
+
+
+/**
+ * {{{
+ * record BooleanTypeData extends PrimitiveTypeData { override supertypes: list[BooleanTypeRef] }
+ * }}}
+ */
+trait BooleanTypeData extends PrimitiveTypeData with RecordDatum[BooleanTypeData]
+
+
+object BooleanTypeData extends RecordType[BooleanTypeData](ns \ "BooleanTypeData", Seq(PrimitiveTypeData), true) {
+
+  val _supertypes: DatumField[ListDatum[BooleanTypeRef]] = field("supertypes", BooleanTypeRef.listOf)
+
+  override lazy val declaredFields: DeclaredFields = DeclaredFields(_supertypes)
+
+}
+
+
+/**
+ * {{{
+ * vartype BooleanTypeRef extends PrimitiveTypeRef { override type: BooleanTypeData }
+ * }}}
+ */
+trait BooleanTypeRef extends PrimitiveTypeRef with Var[BooleanTypeRef]
+
+
+object BooleanTypeRef extends MultiVarType[BooleanTypeRef](ns \ "BooleanTypeRef", Seq(DataTypeRef)) {
+
+  val _type: Tag[TypeRef.`"type"`.type, BooleanTypeData] = tag(TypeRef.`"type"`, BooleanTypeData)
+
+  override lazy val declaredVarTags: DeclaredTags = DeclaredTags(_type)
+
+}
+
