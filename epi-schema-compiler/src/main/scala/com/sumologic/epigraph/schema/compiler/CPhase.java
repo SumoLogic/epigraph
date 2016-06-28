9c707ce57b03baa8fe714c65881b22b4831632d3
Yegor Borovikov <yegor@sumologic.com>
2016-06-28 13:41:03 -0700

    Adding compiler phase enum

---
+++ epi-schema-compiler/src/main/scala/com/sumologic/epigraph/schema/compiler/CPhase.java
@@ -0,0 +1,16 @@
+package com.sumologic.epigraph.schema.compiler;/* Created by yegor on 6/27/16. */
+
+public enum CPhase {
+
+  PARSE,
+  RESOLVE_TYPEREFS,
+  COMPUTE_SUPERTYPES,
+  INHERIT_FROM_SUPERTYPES;
+
+//  public final int order;
+//
+//  CPhase(int order) {
+//    this.order = order;
+//  }
+
+}
