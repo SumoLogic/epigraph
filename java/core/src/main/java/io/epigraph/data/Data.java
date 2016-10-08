bd854baa0dd4187427370fc5350811acbdc4d841
Yegor Borovikov <yegor@sumologic.com>
2016-10-07 17:19:14 -0700

    Adding Data .hashCode() & .equals()

--- java/core/src/main/java/io/epigraph/data/Data.java
+++ java/core/src/main/java/io/epigraph/data/Data.java
@@ -10,6 +10,7 @@ import org.jetbrains.annotations.Nullable;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Objects;
 import java.util.function.Function;
 
 
@@ -68,9 +69,12 @@ public interface Data { // TODO Var? Union? Values?
 
       private final Map<@NotNull String, @NotNull ? extends Val.Imm> tagValues; // TODO optimize for single-tag types
 
+      private final int hashCode;
+
       protected Raw(@NotNull Data.Builder.Raw mutable) {
         super(mutable.type());
         tagValues = Unmodifiable.map(mutable.tagValues(), k -> k, Val::toImmutable);
+        hashCode = Objects.hash(type(), tagValues);
       }
 
       @Override
@@ -101,6 +105,18 @@ public interface Data { // TODO Var? Union? Values?
         return value == null ? null : value.getError();
       }
 
+      @Override
+      public final boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Data)) return false;
+        if (o instanceof Immutable && hashCode != o.hashCode()) return false;
+        Data that = (Data) o;
+        return type().equals(that.type()) && tagValues.equals(that._raw().tagValues());
+      }
+
+      @Override
+      public final int hashCode() { return hashCode; }
+
     }
 
 
@@ -125,6 +141,12 @@ public interface Data { // TODO Var? Union? Values?
 
         @Override
         public final @NotNull Data.Imm.Raw _raw() { return raw; }
+
+        @Override
+        public final int hashCode() { return raw.hashCode(); }
+
+        @Override
+        public final boolean equals(Object obj) { return raw.equals(obj); }
 
       }
 
@@ -198,6 +220,17 @@ public interface Data { // TODO Var? Union? Values?
         setValue(tag, tag.type.createValue(error));
       }
 
+      @Override
+      public final boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Data)) return false;
+        Data that = (Data) o;
+        return type().equals(that.type()) && tagValues.equals(that._raw().tagValues());
+      }
+
+      @Override
+      public final int hashCode() { return Objects.hash(type(), tagValues); }
+
     }
 
 
@@ -208,7 +241,7 @@ public interface Data { // TODO Var? Union? Values?
       private final @NotNull Function<Data.Imm.Raw, MyImmData> immDataConstructor;
 
       protected Static(
-          @NotNull Type type,
+          @NotNull Type type, // TODO extract type from raw
           @NotNull Data.Builder.Raw raw,
           @NotNull Function<Data.Imm.Raw, MyImmData> immDataConstructor
       ) {
@@ -224,116 +257,16 @@ public interface Data { // TODO Var? Union? Values?
       @Override
       public final @NotNull Data.Builder.Raw _raw() { return raw; }
 
+      @Override
+      public final int hashCode() { return raw.hashCode(); }
+
+      @Override
+      public final boolean equals(Object obj) { return raw.equals(obj); }
+
     }
 
 
   }
-
-
-//  abstract class Mut extends Data.Impl implements Mutable {
-//
-//    @Override
-//    public abstract @NotNull Data.Mut.Raw _raw();
-//
-//    protected Mut(@NotNull Type type) { super(type); }
-//
-//
-//    public static final class Raw extends Data.Mut implements Data.Raw {
-//
-//      private final @NotNull Map<String, Val.@NotNull Mut> tagValues = new HashMap<>(); // TODO optimize for single-tag types
-//
-//      private @Nullable Map<String, @NotNull ? extends Val.Mut> unmodifiableViewOfTagValues = null;
-//
-//      public Raw(@NotNull Type type) { super(type); }
-//
-//      @Override
-//      public @NotNull Data.Imm.Raw toImmutable() { return new Data.Imm.Raw(type(), this); }
-//
-//      @Override
-//      public @NotNull Data.Mut.Raw _raw() { return this; }
-//
-//      @Override
-//      public @NotNull Map<@NotNull String, @NotNull ? extends Val.Mut> tagValues() {
-//        if (unmodifiableViewOfTagValues == null) unmodifiableViewOfTagValues = Unmodifiable.map(tagValues);
-//        return unmodifiableViewOfTagValues;
-//      }
-//
-//      @Override
-//      public @Nullable Val.Mut getValue(@NotNull Type.Tag tag) {
-//        // TODO check tag compatibility with this.type
-//        return tagValues.get(tag.name);
-//      }
-//
-//      @Override
-//      public @Nullable Datum getDatum(@NotNull Type.Tag tag) {
-//        Val.Mut value = getValue(tag);
-//        return value == null ? null : value.getDatum();
-//      }
-//
-//      @Override
-//      public @Nullable ErrorValue getError(@NotNull Type.Tag tag) {
-//        Val.Mut value = getValue(tag);
-//        return value == null ? null : value.getError();
-//      }
-//
-//      // TODO accept Value and auto-convert? convert on write (NO - someone might hold the reference already)?
-//      public void setValue(@NotNull Type.Tag tag, @Nullable Val.Mut value) {
-//        // TODO check tag compatibility with this.type
-//        if (value == null) {
-//          tagValues.remove(tag.name);
-//        } else {
-//          // TODO check value compatibility with the tag
-//          tagValues.put(tag.name, value);
-//        }
-//      }
-//
-//      // TODO take Datum and auto-convert?
-//      public void setDatum(@NotNull Type.Tag tag, @Nullable Datum.Mut datum) {
-//        getOrCreateTagValue(tag)._raw().setDatum(datum);
-//      }
-//
-//      public void setError(@NotNull Type.Tag tag, @NotNull ErrorValue error) {
-//        getOrCreateTagValue(tag).setError(error);
-//      }
-//
-//      public @NotNull Val.Mut getOrCreateTagValue(@NotNull Type.Tag tag) {
-//        // TODO check tag compatibility with this.type
-//        Val.Mut value = getValue(tag);
-//        // TODO this (as many other places) is not thread-safe - use ConcurrentHashMap?
-//        if (value == null) tagValues.put(tag.name, value = tag.createMutableValue());
-//        return value;
-//      }
-//
-//    }
-//
-//
-//    public static abstract class Static<MyImmData extends Data.Imm.Static> extends Data.Mut implements Data.Static {
-//
-//      private final @NotNull Data.Mut.Raw raw;
-//
-//      private final @NotNull Function<Data.Imm.Raw, MyImmData> immDataConstructor;
-//
-//      protected Static(
-//          @NotNull Type type,
-//          @NotNull Data.Mut.Raw raw,
-//          @NotNull Function<Data.Imm.Raw, MyImmData> immDataConstructor
-//      ) {
-//        super(type);
-//        // TODO check raw type equality to static type
-//        this.raw = raw;
-//        this.immDataConstructor = immDataConstructor;
-//      }
-//
-//      @Override
-//      public final @NotNull MyImmData toImmutable() { return immDataConstructor.apply(_raw().toImmutable()); }
-//
-//      @Override
-//      public final @NotNull Data.Mut.Raw _raw() { return raw; }
-//
-//    }
-//
-//
-//  }
 
 
 }
