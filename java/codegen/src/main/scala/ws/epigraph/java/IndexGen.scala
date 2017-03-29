0d097623ca4064ce0f99bb88646963467a9c66b8
Konstantin Sobolev <konstantin@sumologic.com>
2017-03-29 11:08:46 -0700

    Revert "`IndexGen` changed to generate `Map<String,Type>` instead of `Map<String,? extends Type>`"
    This reverts commit df5f96f6639a30e823d9b905d97d561f24db3e14.

--- java/codegen/src/main/scala/ws/epigraph/java/IndexGen.scala
+++ java/codegen/src/main/scala/ws/epigraph/java/IndexGen.scala
@@ -1,19 +1,3 @@
-/*
- * Copyright 2017 Sumo Logic
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package ws.epigraph.java
 
 import java.nio.file.{Path, Paths}
@@ -46,11 +30,11 @@ import java.util.Map;
 
 public final class $IndexClassName {
 
-  public static final @NotNull Map<@NotNull String, @NotNull Type> types = types();
+  public static final @NotNull Map<@NotNull String, @NotNull ? extends Type> types = types();
 
   private $IndexClassName() {}
 
-  private static @NotNull Map<@NotNull String, @NotNull Type> types() {
+  private static @NotNull Map<@NotNull String, @NotNull ? extends Type> types() {
     Map<@NotNull String, @NotNull Type> types = new LinkedHashMap<>();
 
 ${ctx.generatedTypes.asScala.toSeq./*TODO better*/sortWith((a, b) => a._1.name < b._1.name).map { entry => sn"""\
@@ -59,7 +43,7 @@ ${ctx.generatedTypes.asScala.toSeq./*TODO better*/sortWith((a, b) => a._1.name <
   }.mkString
 }\
 
-    return Unmodifiable.map_(types);
+    return Unmodifiable.map(types);
   }
 
 }
