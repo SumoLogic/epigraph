32511fef096107ae8158f98dee3d189c55d7cd26
Konstantin Sobolev <konstantin@sumologic.com>
2017-12-15 11:32:57 -0800

    reformatted to allow breakpoints on `return false`

--- java/core/src/main/java/ws/epigraph/data/DataComparator.java
+++ java/core/src/main/java/ws/epigraph/data/DataComparator.java
@@ -44,8 +44,10 @@ public final class DataComparator {
   public static boolean equals(@Nullable Datum d1, @Nullable Datum d2) { return new DataComparator().equal(d1, d2); }
 
   private boolean equal(@Nullable Data d1, @Nullable Data d2) {
-    if (d1 == d2) return true;
-    if (d1 == null || d2 == null) return false;
+    if (d1 == d2)
+      return true;
+    if (d1 == null || d2 == null)
+      return false;
 
     Set<Data> v = visited.get(d1);
 
@@ -60,16 +62,20 @@ public final class DataComparator {
     v.add(d2);
 
     try {
-      if (!d1.type().equals(d2.type())) return false;
+      if (!d1.type().equals(d2.type()))
+        return false;
 
       final Map<@NotNull String, @NotNull ? extends Val> tv1 = d1._raw().tagValues();
       final Map<@NotNull String, @NotNull ? extends Val> tv2 = d2._raw().tagValues();
 
-      if (tv1.size() != tv2.size()) return false;
-      if (!tv1.keySet().equals(tv2.keySet())) return false;
+      if (tv1.size() != tv2.size())
+        return false;
+      if (!tv1.keySet().equals(tv2.keySet()))
+        return false;
 
       for (final Map.Entry<String, ? extends Val> entry : tv1.entrySet()) {
-        if (!equal(entry.getValue(), tv2.get(entry.getKey()))) return false;
+        if (!equal(entry.getValue(), tv2.get(entry.getKey())))
+          return false;
       }
 
       return true;
@@ -83,19 +89,23 @@ public final class DataComparator {
 
   private boolean equal(@Nullable Val v1, @Nullable Val v2) {
     if (v1 == v2) return true;
-    if (v1 == null || v2 == null) return false;
+    if (v1 == null || v2 == null)
+      return false;
 
     //noinspection SimplifiableIfStatement
-    if (!Objects.equals(v1.getError(), v2.getError())) return false;
+    if (!Objects.equals(v1.getError(), v2.getError()))
+      return false;
 
     return equal(v1.getDatum(), v2.getDatum());
   }
 
   private boolean equal(@Nullable Datum d1, @Nullable Datum d2) {
     if (d1 == d2) return true;
-    if (d1 == null || d2 == null) return false;
+    if (d1 == null || d2 == null)
+      return false;
 
-    if (!d1.type().equals(d2.type())) return false;
+    if (!d1.type().equals(d2.type()))
+      return false;
 
     switch (d1.type().kind()) {
       case ENTITY:
@@ -119,38 +129,46 @@ public final class DataComparator {
     final Map<@NotNull String, @NotNull ? extends Data> fd1 = d1._raw().fieldsData();
     final Map<@NotNull String, @NotNull ? extends Data> fd2 = d2._raw().fieldsData();
 
-    if (fd1.size() != fd2.size()) return false;
-    if (!fd1.keySet().equals(fd2.keySet())) return false;
+    if (fd1.size() != fd2.size())
+      return false;
+    if (!fd1.keySet().equals(fd2.keySet()))
+      return false;
 
     for (final Map.Entry<String, ? extends Data> entry : fd1.entrySet()) {
-      if (!equal(entry.getValue(), fd2.get(entry.getKey()))) return false;
+      if (!equal(entry.getValue(), fd2.get(entry.getKey())))
+        return false;
     }
 
     return true;
   }
 
   private boolean equal(@NotNull MapDatum d1, @NotNull MapDatum d2) {
-    if (d1.size() != d2.size()) return false;
+    if (d1.size() != d2.size())
+      return false;
 
     final Map<Datum.@NotNull Imm, @NotNull ? extends Data> e1 = d1._raw().elements();
     final Map<Datum.@NotNull Imm, @NotNull ? extends Data> e2 = d2._raw().elements();
 
-    if (!e1.keySet().equals(e2.keySet())) return false; // keys are immutable and cannot be recursive
+    if (!e1.keySet().equals(e2.keySet()))
+      return false; // keys are immutable and cannot be recursive
 
     for (final Map.Entry<Datum.Imm, ? extends Data> entry : e1.entrySet()) {
-      if (!equal(entry.getValue(), e2.get(entry.getKey()))) return false;
+      if (!equal(entry.getValue(), e2.get(entry.getKey())))
+        return false;
     }
 
     return true;
   }
 
   private boolean equal(@NotNull ListDatum d1, @NotNull ListDatum d2) {
-    if (d1.size() != d2.size()) return false;
+    if (d1.size() != d2.size())
+      return false;
 
     final Iterator<@NotNull ? extends Data> e1 = d1._raw().elements().iterator();
     final Iterator<@NotNull ? extends Data> e2 = d2._raw().elements().iterator();
 
-    while (e1.hasNext()) if (!equal(e1.next(), e2.next())) return false;
+    while (e1.hasNext()) if (!equal(e1.next(), e2.next()))
+      return false;
 
     return true;
   }
